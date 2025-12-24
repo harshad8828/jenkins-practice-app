8433b5bb04745f04ed24095151c05c4daed10c61
Ubuntu <ubuntu@ip-172-31-36-172.ap-south-1.compute.internal>
2025-12-24 12:11:28 +0000

    Testing GitHub Webhook trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Jenkins CI/CD Practice App is Running!\n');
+  res.end('Jenkins Auto-Pilot is LIVE! Build #10 triggered by GitHub.');
 });
 
 const PORT = 3000;
